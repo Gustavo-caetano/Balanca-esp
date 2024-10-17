c4187a86573e4566c72e96b52eaa9c8a4c730405
Gustavo Caetano Santos <73549261+Gustavo-caetano@users.noreply.github.com>
2024-10-16 22:12:00 -0300

    feat:adiciona de maneira provisoria a opcao de calibracao de balanca

--- src/main.cpp
+++ src/main.cpp
@@ -13,19 +13,22 @@ float calibration_factor = 277560.00;
 float count = 0;
 bool crescente = true;
 
+void menu(String opcao);
+void alteracaoDeCalibracao();
+
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Inicializando...");
+  balanca.iniciar();
+  alteracaoDeCalibracao();
 
   wifi.init(
-    "PROJETO1",
-    "umdoistres"
-  );
+      "PROJETO1",
+      "umdoistres");
 
-  socket.iniciar(
-      "ws://10.0.0.16:15000");
-  balanca.iniciar();
+  socket.iniciar("ws://10.0.0.12:15000");
+  // socket.onMenssage(menu);
 }
 
 void loop()
@@ -38,4 +41,40 @@ void loop()
     socket.sendData(peso, balanca.isActive());
   }
   delay(20);
+}
+
+void menu(String opcao)
+{
+  switch (std::stoi(opcao.c_str()))
+  {
+  case 1:
+
+    break;
+
+  default:
+    break;
+  }
+}
+
+void alteracaoDeCalibracao()
+{
+  String calibracao;
+  do
+  {
+    Serial.println("Calibracao informe o modelo:\nA{numero} para aumentar o numero\nD{numero} para diminuir o numero\nPara sair aperte o 0");
+    Serial.setTimeout(700000);
+    calibracao = Serial.readStringUntil('\n');
+    if (calibracao[0] == 'A')
+    {
+      Serial.println("Aumentou");
+      calibration_factor += std::stoi(calibracao.substring(1, calibracao.length()).c_str());
+    }
+    else if (calibracao[0] == 'B')
+    {
+      Serial.println("Diminuiu");
+      calibration_factor -= std::stoi(calibracao.substring(1, calibracao.length()).c_str());
+    }
+    float peso = balanca.getPeso();
+    Serial.println(peso);
+  } while (calibracao != "0");
 }
